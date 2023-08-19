0e7f2377ba4b3f004c2a5cabbdd5798fc6903563
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-08-19 20:05:34 -0300

    fix(test): alteração nos testes que tinham data(String->Date) & diminui o escopo do teste de data invalida

--- gerenciadordetarefas/src/test/java/com/gerenciadordetarefas/GerenciadorDeTarefasTest.java
+++ gerenciadordetarefas/src/test/java/com/gerenciadordetarefas/GerenciadorDeTarefasTest.java
@@ -4,18 +4,23 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 
 public class GerenciadorDeTarefasTest {
 
   @Test
-  public void testCriarTarefaComInformacoesCompletas() {
+  public void testCriarTarefaComInformacoesCompletas() throws ParseException {
       GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
 
       // Definir as informações completas para a tarefa
       String titulo = "Tarefa Importante";
       String descricao = "Realizar a apresentação para a equipe.";
-      String dataVencimento = "2023-08-25";
+      Date dataVencimento = new SimpleDateFormat("yyyy-MM-dd").parse("2023-08-25");
       Prioridade prioridade = Prioridade.ALTA;
 
       // Criar a tarefa usando o método do gerenciador de tarefas
@@ -39,25 +44,20 @@ public class GerenciadorDeTarefasTest {
         GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
 
         // Tentar criar uma tarefa com campos em branco
-        Tarefa tarefa = gerenciador.criarTarefa("", "", "", Prioridade.ALTA);
+        Tarefa tarefa = gerenciador.criarTarefa("", "", null, Prioridade.ALTA);
 
         // Verificar se a tarefa não foi criada
         assertNull("A tarefa deveria ser nula devido aos campos em branco", tarefa);
     }
 
     @Test
-    public void testCriarTarefaComDataInvalida() {
+    public void testCriarTarefaComDataInvalida() throws ParseException {
         GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
 
-        String dataVencimentoPassado = "2023-07-01"; // Exemplo de data no passado
-        String dataVencimentoInvalida = "2023-02-30"; // Exemplo de data inválida (não existe)
+        Date dataVencimentoPassado = new SimpleDateFormat("yyyy-MM-dd").parse("2023-07-01");
 
         // Tentar criar uma tarefa com data de vencimento no passado
         Tarefa tarefaPassado = gerenciador.criarTarefa("Título", "Descrição", dataVencimentoPassado, Prioridade.ALTA);
         assertNull("A tarefa deveria ser nula devido à data de vencimento no passado", tarefaPassado);
-
-        // Tentar criar uma tarefa com data de vencimento inválida
-        Tarefa tarefaInvalida = gerenciador.criarTarefa("Título", "Descrição", dataVencimentoInvalida, Prioridade.ALTA);
-        assertNull("A tarefa deveria ser nula devido à data de vencimento inválida", tarefaInvalida);
     }
 }
