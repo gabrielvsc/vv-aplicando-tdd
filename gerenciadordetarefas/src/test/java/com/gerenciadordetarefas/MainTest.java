7e31f91fa97d5b723a499b0af590876f7ed7bc28
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-08-19 18:37:47 -0300

    build: configuração inicial do projeto com Maven e JUnit

---
+++ gerenciadordetarefas/src/test/java/com/gerenciadordetarefas/MainTest.java
@@ -0,0 +1,12 @@
+package com.gerenciadordetarefas;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MainTest {
+
+  @Test
+  public void exampleTest() {
+    assertTrue(true);
+  }
+}
