3adc3f5c53b87c54b46039886a7db3174654f9c5
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-08-19 23:48:05 -0300

    fix(test): correção de trechos que quebraram devido à mudança de arquivos

--- gerenciadordetarefas/src/test/java/com/gerenciadordetarefas/GerenciadorDeTarefasTest.java
+++ gerenciadordetarefas/src/test/java/com/gerenciadordetarefas/GerenciadorDeTarefasTest.java
@@ -11,6 +11,9 @@ import java.util.Date;
 
 import org.junit.Test;
 
+import com.gerenciadordetarefas.enums.Prioridade;
+import com.gerenciadordetarefas.model.Tarefa;
+
 public class GerenciadorDeTarefasTest {
 
   // 1. Testes de Criar Tarefa
@@ -25,13 +28,13 @@ public class GerenciadorDeTarefasTest {
       Prioridade prioridade = Prioridade.ALTA;
 
       // Criar a tarefa usando o método do gerenciador de tarefas
-      Tarefa tarefa = gerenciador.criarTarefa(titulo, descricao, dataVencimento, prioridade);
+      Tarefa tarefa = gerenciador.gerenciadorDeTarefasService.criarTarefa(titulo, descricao, dataVencimento, prioridade);
 
       // Verificar se a tarefa foi criada com sucesso
       assertNotNull("A tarefa não deveria ser 'null'", tarefa);
 
       // Verificar se a tarefa está presente na lista de tarefas do gerenciador
-      assertTrue("A tarefa não está presente na lista", gerenciador.listaDeTarefas().contains(tarefa));
+      assertTrue("A tarefa não está presente na lista", gerenciador.gerenciadorDeTarefasService.listaDeTarefas().contains(tarefa));
 
       // Verificar se os detalhes da tarefa estão corretos
       assertEquals("Título incorreto", titulo, tarefa.getTitulo());
@@ -45,7 +48,7 @@ public class GerenciadorDeTarefasTest {
         GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
 
         // Tentar criar uma tarefa com campos em branco
-        Tarefa tarefa = gerenciador.criarTarefa("", "", null, Prioridade.ALTA);
+        Tarefa tarefa = gerenciador.gerenciadorDeTarefasService.criarTarefa("", "", null, Prioridade.ALTA);
 
         // Verificar se a tarefa não foi criada
         assertNull("A tarefa deveria ser nula devido aos campos em branco", tarefa);
@@ -58,7 +61,7 @@ public class GerenciadorDeTarefasTest {
         Date dataVencimentoPassado = new SimpleDateFormat("yyyy-MM-dd").parse("2023-07-01");
 
         // Tentar criar uma tarefa com data de vencimento no passado
-        Tarefa tarefaPassado = gerenciador.criarTarefa("Título", "Descrição", dataVencimentoPassado, Prioridade.ALTA);
+        Tarefa tarefaPassado = gerenciador.gerenciadorDeTarefasService.criarTarefa("Título", "Descrição", dataVencimentoPassado, Prioridade.ALTA);
         assertNull("A tarefa deveria ser nula devido à data de vencimento no passado", tarefaPassado);
     }
 
@@ -69,7 +72,7 @@ public class GerenciadorDeTarefasTest {
         Date dataVencimento = new Date(System.currentTimeMillis() + 86400000); // Data de amanhã
         
         // Tentar criar uma tarefa com prioridade inválida
-        Tarefa tarefaInvalida = gerenciador.criarTarefa("Título", "Descrição", dataVencimento, null);
+        Tarefa tarefaInvalida = gerenciador.gerenciadorDeTarefasService.criarTarefa("Título", "Descrição", dataVencimento, null);
         assertNull("A tarefa deveria ser nula devido à prioridade inválida", tarefaInvalida);
     }
     
@@ -77,18 +80,18 @@ public class GerenciadorDeTarefasTest {
 
     @Test
     public void testAtualizarTituloTarefa() {
-        GerenciadorDeTarefasService gerenciadorService = new GerenciadorDeTarefasService();
+        GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
 
         Date dataVencimento = new Date(System.currentTimeMillis() + 86400000); // Data de amanhã
 
         // Criar uma tarefa inicial com um título
-        Tarefa tarefaInicial = gerenciadorService.criarTarefa("Título Inicial", "Descrição", dataVencimento, Prioridade.ALTA);
+        Tarefa tarefaInicial = gerenciador.gerenciadorDeTarefasService.criarTarefa("Título Inicial", "Descrição", dataVencimento, Prioridade.ALTA);
         assertNotNull("A tarefa inicial não deveria ser nula", tarefaInicial);
         assertEquals("Título inicial incorreto", "Título Inicial", tarefaInicial.getTitulo());
 
         // Atualizar o título da tarefa utilizando o serviço
         String novoTitulo = "Novo Título";
-        gerenciadorService.atualizarTituloTarefa(tarefaInicial, novoTitulo);
+        gerenciador.gerenciadorDeTarefasService.atualizarTituloTarefa(tarefaInicial, novoTitulo);
         assertEquals("Título não foi atualizado corretamente", novoTitulo, tarefaInicial.getTitulo());
     }
 }
