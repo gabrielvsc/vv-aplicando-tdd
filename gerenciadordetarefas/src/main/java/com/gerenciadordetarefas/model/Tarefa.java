e8865bdbd733686c40abc048ae4712062e73dc80
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-08-20 09:52:01 -0300

    feat(tarefa): adiciono o atributo id e ajusto o tab size (4->2)

--- gerenciadordetarefas/src/main/java/com/gerenciadordetarefas/model/Tarefa.java
+++ gerenciadordetarefas/src/main/java/com/gerenciadordetarefas/model/Tarefa.java
@@ -1,51 +1,63 @@
 package com.gerenciadordetarefas.model;
 
 import java.util.Date;
+import java.util.UUID;
 
 import com.gerenciadordetarefas.enums.Prioridade;
 
 public class Tarefa {
-    private String titulo;
-    private String descricao;
-    private Date dataDeVencimento;
-    private Prioridade prioridade;
 
-    public Tarefa(String titulo, String descricao, Date dataDeVencimento, Prioridade prioridade) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.dataDeVencimento = dataDeVencimento;
-        this.prioridade = prioridade;
-    }
+  private String id;
+  private String titulo;
+  private String descricao;
+  private Date dataDeVencimento;
+  private Prioridade prioridade;
 
-    public String getTitulo() {
-      return titulo;
-    }
+  public Tarefa(String titulo, String descricao, Date dataDeVencimento, Prioridade prioridade) {
+    this.id = UUID.randomUUID().toString();  
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.dataDeVencimento = dataDeVencimento;
+    this.prioridade = prioridade;
+  }
 
-    public void setTitulo(String titulo) {
-      this.titulo = titulo;
-    }
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
     
-    public String getDescricao() {
-      return descricao;
-    }
+  public String getDescricao() {
+    return descricao;
+  }
 
-    public void setDescricao(String descricao) {
-      this.descricao = descricao;
-    }
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
 
-    public Date getDataDeVencimento() {
-      return dataDeVencimento;
-    }
+  public Date getDataDeVencimento() {
+    return dataDeVencimento;
+  }
 
-    public void setDataDeVencimento(Date dataDeVencimento) {
-      this.dataDeVencimento = dataDeVencimento;
-    }
+  public void setDataDeVencimento(Date dataDeVencimento) {
+    this.dataDeVencimento = dataDeVencimento;
+  }
 
-    public Prioridade getPrioridade() {
-      return prioridade;
-    }
+  public Prioridade getPrioridade() {
+    return prioridade;
+  }
 
-    public void setPrioridade(Prioridade prioridade) {
-      this.prioridade = prioridade;
-    }
+  public void setPrioridade(Prioridade prioridade) {
+    this.prioridade = prioridade;
+  }
 }
