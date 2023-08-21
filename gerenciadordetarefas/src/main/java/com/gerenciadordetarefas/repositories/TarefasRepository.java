503328d5aaa3fd4c81ab0a97247c5196c92a2de1
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-08-21 17:58:37 -0300

    docs: adicionada documentação no service e repository

--- gerenciadordetarefas/src/main/java/com/gerenciadordetarefas/repositories/TarefasRepository.java
+++ gerenciadordetarefas/src/main/java/com/gerenciadordetarefas/repositories/TarefasRepository.java
@@ -5,26 +5,56 @@ import java.util.List;
 
 import com.gerenciadordetarefas.model.Tarefa;
 
+/**
+ * Repositório de tarefas que mantém uma lista de tarefas.
+ */
 public class TarefasRepository {
 
   private List<Tarefa> tarefas;
 
+  /**
+   * Construtor da classe.
+   *
+   * @param tarefas Uma lista de tarefas para inicializar o repositório.
+   */
   public TarefasRepository(List<Tarefa> tarefas) {
     this.tarefas = new ArrayList<Tarefa>(tarefas);
   }
 
+  /**
+   * Cria uma nova tarefa e a adiciona ao repositório.
+   *
+   * @param tarefa A tarefa a ser criada e adicionada.
+   * @return {@code true} se a tarefa foi adicionada com sucesso, {@code false} caso contrário.
+   */
   public boolean criarTarefa(Tarefa tarefa) {
     return tarefas.add(tarefa);
   }
 
+  /**
+   * Remove uma tarefa do repositório com base no seu ID.
+   *
+   * @param id O ID da tarefa a ser excluída.
+   */
   public void excluirTarefa(String id) {
     tarefas.removeIf(tarefa -> tarefa.getId().equals(id));
   }
 
+  /**
+   * Retorna a lista de todas as tarefas presentes no repositório.
+   *
+   * @return A lista de tarefas.
+   */
   public List<Tarefa> listaDeTarefas() {
     return tarefas;
   }
 
+  /**
+   * Busca uma tarefa no repositório com base no seu ID.
+   *
+   * @param id O ID da tarefa a ser buscada.
+   * @return A tarefa correspondente ao ID, ou {@code null} se não encontrada.
+   */
   public Tarefa findById(String id) {
     for (Tarefa tarefa : tarefas) {
       if (tarefa.getId().equals(id)) {
--- gerenciadordetarefas/src/main/java/com/gerenciadordetarefas/service/GerenciadorDeTarefasServiceImpl.java
+++ gerenciadordetarefas/src/main/java/com/gerenciadordetarefas/service/GerenciadorDeTarefasServiceImpl.java
@@ -10,14 +10,31 @@ import com.gerenciadordetarefas.enums.Prioridade;
 import com.gerenciadordetarefas.model.Tarefa;
 import com.gerenciadordetarefas.repositories.TarefasRepository;
 
+/**
+ * Implementação do serviço de gerenciamento de tarefas.
+ */
 public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasService {
 
   private TarefasRepository tarefasRepository;
 
+  /**
+   * Construtor da classe.
+   *
+   * @param tarefasRepository Repositório de tarefas a ser utilizado.
+   */
   public GerenciadorDeTarefasServiceImpl(TarefasRepository tarefasRepository) {
     this.tarefasRepository = tarefasRepository;
   }
 
+  /**
+   * Cria uma nova tarefa e a adiciona ao repositório de tarefas.
+   *
+   * @param titulo O título da nova tarefa.
+   * @param descricao A descrição da nova tarefa.
+   * @param dataDeVencimento A data de vencimento da nova tarefa.
+   * @param prioridade A prioridade da nova tarefa.
+   * @return A nova tarefa criada, ou null se a tarefa não for válida.
+   */
   public Tarefa criarTarefa(String titulo, String descricao, Date dataDeVencimento, Prioridade prioridade) {
     if (!isValidTarefa(new Tarefa(titulo, descricao, dataDeVencimento, prioridade))) {
       return null;
@@ -28,6 +45,13 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return novaTarefa;
   }
 
+  /**
+   * Atualiza o título de uma tarefa.
+   *
+   * @param id O ID da tarefa a ser atualizada.
+   * @param novoTitulo O novo título para a tarefa.
+   * @return true se o título foi atualizado com sucesso, false caso contrário.
+   */
   public boolean atualizarTituloTarefa(String id, String novoTitulo) {
     Tarefa tarefa = getTarefa(id);
     if (!isNullOrEmpty(novoTitulo)) {
@@ -37,6 +61,13 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return false;
   }
 
+  /**
+   * Atualiza a data de vencimento de uma tarefa.
+   *
+   * @param id O ID da tarefa a ser atualizada.
+   * @param novaDataDeVencimento A nova data de vencimento para a tarefa.
+   * @return true se a data de vencimento foi atualizada com sucesso, false caso contrário.
+   */
   public boolean atualizarDataDeVencimentoTarefa(String id, Date novaDataDeVencimento) {
     Tarefa tarefa = getTarefa(id);
     if (!isNullOrEmpty(novaDataDeVencimento.toString()) && isValidDate(novaDataDeVencimento)) {
@@ -46,6 +77,13 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return false;
   }
 
+  /**
+   * Atualiza a prioridade de uma tarefa.
+   *
+   * @param id O ID da tarefa a ser atualizada.
+   * @param prioridade A nova prioridade para a tarefa.
+   * @return true se a prioridade foi atualizada com sucesso, false caso contrário.
+   */
   public boolean atualizarPrioridadeTarefa(String id, Prioridade prioridade) {
     Tarefa tarefa = getTarefa(id);
     if (!isNullOrEmpty(prioridade.toString())) {
@@ -55,6 +93,13 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return false;
   }
 
+  /**
+   * Atualiza a descrição de uma tarefa.
+   *
+   * @param id O ID da tarefa a ser atualizada.
+   * @param novaDescricao A nova descrição para a tarefa.
+   * @return true se a descrição foi atualizada com sucesso, false caso contrário.
+   */
   public boolean atualizarDescricaoTarefa(String id, String novaDescricao) {
     Tarefa tarefa = getTarefa(id);
     if (!isNullOrEmpty(novaDescricao)) {
@@ -64,6 +109,12 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return false;
   }
 
+  /**
+   * Exclui uma tarefa.
+   *
+   * @param id O ID da tarefa a ser excluída.
+   * @return true se a tarefa foi excluída com sucesso, false caso contrário.
+   */
   public boolean excluirTarefa(String id) {
     Tarefa tarefa = getTarefa(id);
     if (tarefa != null) {
@@ -73,10 +124,13 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return false;
   }
 
+  /**
+   * Obtém a lista de tarefas ordenada por data de vencimento e prioridade.
+   *
+   * @return Lista de tarefas ordenada.
+   */
   public List<Tarefa> listaDeTarefas() {
     List<Tarefa> tarefas = tarefasRepository.listaDeTarefas();
-
-    // Ordenar a lista de tarefas por data de vencimento e depois por prioridade
     Collections.sort(tarefas, new Comparator<Tarefa>() {
       public int compare(Tarefa tarefa1, Tarefa tarefa2) {
         int comparison = tarefa1.getDataDeVencimento().compareTo(tarefa2.getDataDeVencimento());
@@ -89,11 +143,24 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return tarefas;
   }
 
+  /**
+   * Verifica se uma data é válida, ou seja, não está antes da data atual.
+   *
+   * @param date A data a ser verificada.
+   * @return true se a data for válida, false caso contrário.
+   */
   public boolean isValidDate(Date date) {
     Date dataAtual = new Date();
     return !date.before(dataAtual);
   }
 
+  /**
+   * Obtém uma tarefa pelo seu ID.
+   *
+   * @param id O ID da tarefa.
+   * @return A tarefa correspondente ao ID.
+   * @throws NoSuchElementException Se a tarefa não for encontrada.
+   */
   public Tarefa getTarefa(String id) throws NoSuchElementException {
     Tarefa tarefa = tarefasRepository.findById(id);
     if (tarefa == null) {
@@ -102,10 +169,22 @@ public class GerenciadorDeTarefasServiceImpl implements GerenciadorDeTarefasServ
     return tarefa;
   }
 
+  /**
+   * Verifica se uma string é nula ou vazia.
+   *
+   * @param value A string a ser verificada.
+   * @return true se a string for nula ou vazia, false caso contrário.
+   */
   private boolean isNullOrEmpty(String value) {
     return value == null || value.isEmpty();
   }
 
+  /**
+   * Verifica se uma tarefa é válida.
+   *
+   * @param tarefa A tarefa a ser verificada.
+   * @return true se a tarefa for válida, false caso contrário.
+   */
   private boolean isValidTarefa(Tarefa tarefa) {
     return tarefa != null && 
       !isNullOrEmpty(tarefa.getTitulo()) &&
